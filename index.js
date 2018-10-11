d9bf1ddf3fba0603eb3bd3eca6a8300cee8cb2fc
tedy <tedy@r2v.io>
2018-10-11 18:34:42 +0900

    Edit transaction timestamp data

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ if (typeof ygg !== 'undefined') {
 
 /* stem */
 
-var owner = '0xA771A6b5A6cAbE2ca35Fd55631717d95049D6338';
+const timestamp = Math.round(new Date().getTime() / 1000);
 
 var seed = {            
          "name": 'YEED',
@@ -66,32 +66,29 @@ var seed = {           
          "version": '0xcc9612ff91ff844938acdb6608e58506a2f21b8a5d77e88726c0897e8d1d02c0',
          "reference_address": '',
          "reserve_address": '0xcee3d4755e47055b530deeba062c5bd0c17eb00f',
-         "owner": owner,
+         "owner": '0xA771A6b5A6cAbE2ca35Fd55631717d95049D6338',
+         "timestamp": timestamp,
          "version_history":['0xcc9612ff91ff844938acdb6608e58506a2f21b8a5d77e88726c0897e8d1d02c0']
 }
 
 const branch = ygg.client.branch(seed);
 
-let jsonBody = "["+ygg.utils.dataToJson(branch)+"]";  
-
-const bodyHash = ygg.utils.bodyHashHex(jsonBody)
-
-const bodyLength = ygg.utils.decimalToHex(jsonBody.length)
+let jsonBody = ygg.utils.dataToJson(branch);  
 
 const txHeaderData = {
     "chain":`0xfe7b7c93dd23f78e12ad42650595bc0f874c88f7`,
     "version":`0x0000000000000000`,
     "type":`0x0000000000000000`,
-    "timeStamp":`0x${ygg.utils.decimalToHex(Math.round(new Date().getTime() / 1000))}`,
-    "bodyHash": `0x${bodyHash}`,
-    "bodyLength":`0x${bodyLength}`
+    "timeStamp":`0x${ygg.utils.decimalToHex(timestamp)}`,
+    "bodyHash": `0x${ygg.utils.bodyHashHex(jsonBody)}`,
+    "bodyLength":`0x${ygg.utils.decimalToHex(jsonBody.length)}`
   };
 
 let tx = new ygg.tx(txHeaderData);
 
 const signature = tx.sign(new Buffer('3D8A58EA7FA6EF7E038791F3B837FA7BC62DC38CAAFE67AFC4D4567A64D4966E', 'hex'));
 
-let registerData = tx.branchRegister(branch, signature);
+let registerData = tx.branchRegister(branch, signature, timestamp);
 
 let register = ygg.client.register(registerData);
 
