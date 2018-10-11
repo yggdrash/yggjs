2bd73bc92ab1afb961633af8db8a3a4416cd4d67
tedy <tedy@r2v.io>
2018-10-11 16:14:03 +0900

    Add trasfer transaction

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # Yggdrash Javascript SDK
 
 ## Docs
-[Documentation]() soon...
+[Documentation](https://yggdrash.atlassian.net/wiki/spaces/DEV/pages/32768001/SDK)
 
 ### Usage
 ```
+var Yggdrash = require('ygg');
+
 if (typeof ygg !== 'undefined') {
-    ygg = new Ygg(ygg.currentProvider);
+    ygg = new Yggdrash(ygg.currentProvider);
 } else {
-    ygg = new Ygg(new Ygg.providers.HttpProvider('http://localhost:8080'));
+    ygg = new Yggdrash(new Yggdrash.providers.HttpProvider('http://localhost:8080'));
 }
+```
+
+###
+Example
+```
+let branchId = '0xa771a6b5a6cabe2ca35fd55631717d95049d6338';
+let address = 'aca4215631187ab5b3af0d4c251fdf45c79ad3c6';
+
+ygg.client.getBalance(branchId, address);
 ```
 
 ## Installation
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var Ygg = require('./lib/ygg');
-var { toBuffer, dataToJson } = require('./lib/internal/utils');
 
 if (typeof window !== 'undefined' && typeof window.Ygg === 'undefined') {
     window.Ygg = Ygg;
@@ -7,15 +6,53 @@ if (typeof window !== 'undefined' && typeof window.Ygg === 'undefined') {
 
 // module.exports = Ygg;
 
+
+
 if (typeof ygg !== 'undefined') {
     ygg = new Ygg(ygg.currentProvider);
 } else {
-    // Set the provider you want from Web3.providers
     ygg = new Ygg(new Ygg.providers.HttpProvider("http://localhost:8080"));
-    // let val = ygg.client.getBalance('0xa771a6b5a6cabe2ca35fd55631717d95049d6338', '0xa771a6b5a6cabe2ca35fd55631717d95049d6338');
-    // console.log(val);
 }
+/* balance */
 
+// let val = ygg.client.getBalance('0xa771a6b5a6cabe2ca35fd55631717d95049d6338', '0xa771a6b5a6cabe2ca35fd55631717d95049d6338');
+// console.log(val);
+
+
+/* transfer */
+
+// var to = 'aca4215631187ab5b3af0d4c251fdf45c79ad3c6';
+// var amount = 1000000;
+
+// const transfer = ygg.client.transfer(to, amount);
+
+// let jsonBody = ygg.utils.dataToJson(transfer)
+
+// const bodyHash = ygg.utils.bodyHashHex(jsonBody)
+
+// const bodyLength = ygg.utils.decimalToHex(bodyHash.length)
+
+// let branchId = '0xfe7b7c93dd23f78e12ad42650595bc0f874c88f7'
+
+// const txHeaderData = {
+//     "chain":`0xfe7b7c93dd23f78e12ad42650595bc0f874c88f7`,
+//     "version":`0x0000000000000000`,
+//     "type":`0x0000000000000000`,
+//     "timeStamp":`0x${ygg.utils.decimalToHex(Math.round(new Date().getTime() / 1000))}`,
+//     "bodyHash": `0x${bodyHash}`,
+//     "bodyLength":`0x${bodyLength}`
+//   };
+
+// let tx = new ygg.tx(txHeaderData);
+
+// const signature = tx.sign(new Buffer('5654f359c90004451a32dfd0286e61d1944b5a1ecde05808c11138c0c2c26520', 'hex'));
+
+// let registerData = tx.transferSerialize(branchId, 'aca4215631187ab5b3af0d4c251fdf45c79ad3c6', transfer, signature);
+
+// console.log(registerData)
+
+
+/* stem */
 
 var owner = 'aca4215631187ab5b3af0d4c251fdf45c79ad3c6';
 var seed = {            
@@ -34,18 +71,24 @@ var seed = {           
 
 const branch = ygg.client.branch(seed);
 
+let jsonBody = ygg.utils.dataToJson(branch)
+
+const bodyHash = ygg.utils.bodyHashHex(jsonBody)
+
+const bodyLength = ygg.utils.decimalToHex(bodyHash.length)
+
 const txHeaderData = {
     "chain":`0xfe7b7c93dd23f78e12ad42650595bc0f874c88f7`,
     "version":`0x0000000000000000`,
     "type":`0x0000000000000000`,
-    "timeStamp":`0x000000005bbd6570`,
-    "bodyHash": `0x${ygg.utils.bodyHashHex(dataToJson(branch))}`,
-    "bodyLength":`0x000000000000029f`
+    "timeStamp":`0x${ygg.utils.decimalToHex(Math.round(new Date().getTime() / 1000))}`,
+    "bodyHash": `0x${bodyHash}`,
+    "bodyLength":`0x${bodyLength}`
   };
 
 let tx = new ygg.tx(txHeaderData);
 
-const signature = tx.sign(ygg.txUtils.toBuffer(`0x5654f359c90004451a32dfd0286e61d1944b5a1ecde05808c11138c0c2c26520`));
+const signature = tx.sign(new Buffer('5654f359c90004451a32dfd0286e61d1944b5a1ecde05808c11138c0c2c26520', 'hex'));
 
 let registerData = tx.branchRegister(branch, signature);
 
