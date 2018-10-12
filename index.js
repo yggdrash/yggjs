cdcdd1aa519e71123113c1de33d429db253abcee
tedy <tedy@r2v.io>
2018-10-12 11:42:12 +0900

    Edit generate branch data logic

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const timestamp = Math.round(new Date().getTime() / 1000);
 
 // const signature = tx.sign(new Buffer('3D8A58EA7FA6EF7E038791F3B837FA7BC62DC38CAAFE67AFC4D4567A64D4966E', 'hex'));
 
-// let transferData = tx.transferSerialize(branchId, 'A771A6b5A6cAbE2ca35Fd55631717d95049D6338', transfer, signature, timestamp);
+// let transferData = tx.transferSerialize(branchId, 'A771A6b5A6cAbE2ca35Fd55631717d95049D6338', transfer, signature);
 
 // ygg.client.sendTransaction(transferData)
 
@@ -76,10 +76,10 @@ const txHeaderData = {
     "chain":`0xfe7b7c93dd23f78e12ad42650595bc0f874c88f7`,
     "version":`0x0000000000000000`,
     "type":`0x0000000000000000`,
-    "timeStamp":`0x${ygg.utils.decimalToHex(timestamp)}`,
+    "timeStamp":`0x${ygg.utils.decimalToHex(branch.params[0].branch.timestamp)}`,
     "bodyHash": `0x${ygg.utils.bodyHashHex(jsonBody)}`,
     "bodyLength":`0x${ygg.utils.decimalToHex(jsonBody.length)}`
-  };
+};
 
 let tx = new ygg.tx(txHeaderData);
 
