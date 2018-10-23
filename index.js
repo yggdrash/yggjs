42672ca6dee461854960007a199f9b5807452070
tedy <tedy@r2v.io>
2018-10-23 14:36:04 +0900

    Add node restart

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const timestamp = Math.round(new Date().getTime());
 
 let body = ygg.client.nodeHello();
 let bodyJson = ygg.utils.dataToJson(body)
-console.log("serialize", bodyJson)    
+
 const rawTx = {
     "timeStamp":`0x${ygg.utils.decimalToHex(timestamp)}`,
     "nonce":`0x${ygg.utils.nonce()}`,
@@ -38,7 +38,29 @@ let serialize = tx.serialize(body);
 // console.log("pubkey", tx.getSenderPublicKey().toString('hex'))
 
 ygg.client.getNonce(serialize).then((result) => {
-    console.log("nonce", result)    
+    console.log("nonce", result)  
+    
+    // let body = ygg.client.nodeRestart();
+    let body = ygg.client.nodeSetConfig(32921,"info");
+    let bodyJson = ygg.utils.dataToJson(body)
+
+    const rawTx = {
+        "timeStamp":`0x${ygg.utils.decimalToHex(timestamp)}`,
+        "nonce":`0x${ygg.utils.nonce(result)}`,
+        "bodyHash":`0x${ygg.utils.bodyHashHex(bodyJson)}`,    
+        "bodyLength":`0x${ygg.utils.decimalToHex(bodyJson.length)}`
+    };
+
+    let tx = new ygg.tx(rawTx);
+
+    tx.sign(new Buffer('3D8A58EA7FA6EF7E038791F3B837FA7BC62DC38CAAFE67AFC4D4567A64D4966E', 'hex'));
+
+    let serialize = tx.serialize(body);
+    // console.log("serialize", serialize) 
+
+    ygg.client.requestCommand(serialize).then((result) => {
+        console.log("result", result)    
+    })
 })
 
 
