9d9869d79281cfea14a1b8e3b8b6791904883656
tedy <tedy@r2v.io>
2018-11-28 17:43:34 +0900

    Delete test code

--- index.js
+++ index.js
@@ -5,14 +5,3 @@ if (typeof window !== 'undefined' && typeof window.Ygg === 'undefined') {
 }
 
 module.exports = Ygg;
-
-ygg = new Ygg(new Ygg.providers.HttpProvider("http://localhost:8080"));
-const timestamp = new Date().getTime();
-/* get balance */
-// ygg.client.getBalance('0a39170899bd7e721730c7c312afc154d784034b', '0xaca4215631187ab5b3af0d4c251fdf45c79ad3c6').then(v => {
-//     console.log(v)
-// })
-/* get branch */
-ygg.client.getBranchId().then(all => {
-    console.log(all)
-})
