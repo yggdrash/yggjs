18247383e5b3cea487ac9a33f3e7da8c84385fc3
tedy <tedy@r2v.io>
2018-12-12 11:56:01 +0900

    Edit transaction body

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ if (typeof window !== 'undefined' && typeof window.Ygg === 'undefined') {
 
 module.exports = Ygg;
 
-// ygg = new Ygg(new Ygg.providers.HttpProvider('http://localhost:8080'))
-// ygg.client.getBranchId().then((d) => console.log(d))
-// ygg.client.specification('118eaa393958caf8e8c103fa9d78b5d2ded53109').then((d)=>console.log(d))
-// ygg.client.totalSupply('118eaa393958caf8e8c103fa9d78b5d2ded53109').then((d)=>console.log(d))
-// ygg.client.getBalance('118eaa393958caf8e8c103fa9d78b5d2ded53109', 'c91e9d46dd4b7584f0b6348ee18277c10fd7cb94').then((d)=>console.log(d))
-// ygg.client.allowance('118eaa393958caf8e8c103fa9d78b5d2ded53109', 'cee3d4755e47055b530deeba062c5bd0c17eb00f', '1a0cdead3d1d1dbeef848fef9053b4f0ae06db9e').then((d)=>console.log(d))
+
+ygg = new Ygg(new Ygg.providers.HttpProvider('http://localhost:8080'))
+let timestamp = new Date().getTime()
+
+const body = ygg.client.txBody('18D5dACb8f793d7aEA95f24008067D0E004eac11', 1000)
+let bodyJson = ygg.utils.dataToJson(body)
+
+const rawTx = {
+    "chain":`0x118eaa393958caf8e8c103fa9d78b5d2ded53109`,
+    "version":`0x0000000000000000`,
+    "type":`0x0000000000000000`,
+    "timeStamp":`0x${ygg.utils.decimalToHex(timestamp)}`,
+    "bodyHash":`0x${ygg.utils.bodyHashHex(bodyJson)}`,
+    "bodyLength":`0x${ygg.utils.decimalToHex(bodyJson.length)}`
+  };
+
+const tx = new ygg.tx(rawTx)
+tx.sign(Buffer.from('7c2b4f805c3bbce62cffe6e6682334ec39b12ff7946f3390f852713292411865', 'hex'))
+let serialize = tx.serialize(body)
+console.log(serialize)
+
+ygg.client.sendTransaction(serialize).then((result) => {
+    console.log(`  ` + `==> Transaction Hash : ${result}`)
+})
