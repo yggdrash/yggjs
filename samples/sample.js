bafa681d31518a87d75ed58793d125c6afe7a030
Luke Hwang <luke@r2v.io>
2018-12-14 16:40:59 +0900

    Fix error
    - Import syntax to commonjs
    - Apply changed util location

---
+++ samples/sample.js
@@ -0,0 +1,51 @@
+const Buffer = require('safe-buffer').Buffer
+const Yggdrash = require('..')
+const { Transaction } = require('..')
+
+const branchId = 'a5f436a66ce5ca5b7dbd6bbf8460701b8cbf0485';
+
+describe('Version 1.0', () => {
+  it('Transaction', () => {
+    const ygg = new Yggdrash()
+    const body = ygg.client.transferBody('2dbe588da70cafe98bd1797119e96165a8e74191', '1000')
+    let tx = new ygg.tx(txHeader())
+    tx.sign(Buffer.from('310d08df73d4bc989ea82a7002ceb6f60896ebc80feeeb80c04b6a27f9b4985e', 'hex'))
+    let serialize = tx.serialize(body)
+    ygg.client.sendTransaction(serialize).then(res => {
+      console.log(res)
+    })
+  })
+
+  const txHeader = () => {
+    return {
+      "chain":`0xa5f436a66ce5ca5b7dbd6bbf8460701b8cbf0485`,
+      "version":`0x0000000000000000`,
+      "type":`0x0000000000000000`,
+      "timeStamp":`0x0000016786e00bd8`,
+      "bodyHash":`0xe895380ce95a51273d08ef189b8fea3b42f22c20f35ab4a0abff4bc569f94597`,
+      "bodyLength":'0x0000000000000069'
+    };
+  }
+})
+
+describe('Version 2.0', () => {
+  it('new Transaction()', () => {
+    let params = {to: '2dbe588da70cafe98bd1797119e96165a8e74191', amount: '1000'}
+    let tx = new Transaction(branchId, 'transfer', params)
+    tx.sign('310d08df73d4bc989ea82a7002ceb6f60896ebc80feeeb80c04b6a27f9b4985e')
+    tx.send().then(res => {
+      console.log(res)
+    })
+  })
+
+  //To Be
+  /*
+  const YeedBranch = new Branch(branchId)
+  YeedBranch.tx('transfer', {to: '2dbe588da70cafe98bd1797119e96165a8e74191', amount: '1000'})
+    .sign('310d08df73d4bc989ea82a7002ceb6f60896ebc80feeeb80c04b6a27f9b4985e')
+    .send().then(res => {
+      console.log(res)
+  })
+  */
+})
+
--- test/Wallet.spec.js
+++ test/Wallet.spec.js
@@ -1,30 +1,29 @@
 'use strict'
 
-import 'chai/register-should';
-import { expect } from 'chai'
-import ksHelper from '../lib/local/wallet/generation'
-import sinon from 'sinon'
+const { expect } = require('chai')
+const ksHelper = require('../lib/local/wallet/generation')
+const sinon = require('sinon')
 
 const dummy = {
   password: 'password',
   address: '3ebd8c7a48acA9a2e39dCa01D0d83e14B7E66F',
   privateKey: '67e32c0e5bbdb38061fa51cf3d8f239ee6c603517711763bf9aa1d5a706df0f5',
-  keystore: { 
+  keystore: {
       address: 'fb3ebd8c7a48aca9a2e39dca01d0d83e14b7e66f',
-      crypto: { 
+      crypto: {
         ciphertext: 'fa785574b58408c4451c3e8b5b79d1b0c3e3d563ad79b884b0453e6e20708784725b2af234aecf20f5337221f95aacf6',
         cipherparams: { iv: 'cdbbacfed7f921d159157f721f3c3aae' },
         cipher: 'aes-128-cbc',
         kdf: 'pbkdf2',
-        kdfparams: { 
+        kdfparams: {
           dklen: 32,
           salt:
           'bcc41a11f0f9f4c9373a7379cba0a5cb2fb6866cd40f895c949fad4116f5a61f',
           c: 262144,
-          prf: 'hmac-sha256' 
+          prf: 'hmac-sha256'
         },
-        mac: 'ead56f83c014e4d3825319f59e8a4f193620bd78d14b113d24f704f64f901be8' 
-      } 
+        mac: 'ead56f83c014e4d3825319f59e8a4f193620bd78d14b113d24f704f64f901be8'
+      }
     }
 }
 
